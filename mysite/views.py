a2ac7d76ec6162b5d9b7592e3574932b8436e655
Shmele <my@email.ru>
2012-08-31 15:40:15 +0800

    Add template.

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
 
 def hello(request):
-    return HttpResponse("Hello world")
+    t = get_template('template_hello.html')
+    html = t.render(Context({'text':'Hello world!'}))
+
+    return HttpResponse(html)
